9b37f6fb602354e94317804a873fb54df440e397
Ferdian9991 <ferdisatria15@gmail.com>
2024-06-22 11:35:24 +0700

    (add) added the iron block

--- src/common/block-arena/ArenaEasy.js
+++ src/common/block-arena/ArenaEasy.js
@@ -1,4 +1,5 @@
 import GrassBlock from "../blocks/GrassBlock.js";
+import IronBlock from "../blocks/IronBlock.js";
 
 export default class ArenaEasy {
   constructor(id) {
@@ -6,35 +7,35 @@ export default class ArenaEasy {
   }
 
   draw() {
-    new GrassBlock(
+    new IronBlock(
       this.id,
       0,
       window.innerHeight - 48,
       Math.round(window.innerWidth / 48) + 1
     ).draw();
 
-    new GrassBlock(
+    new IronBlock(
       this.id,
       0,
       window.innerHeight - 48 * 5.5,
       Math.round(window.innerWidth / (48 * 2)) + 5
     ).draw();
 
-    new GrassBlock(
+    new IronBlock(
       this.id,
       Math.round(window.innerWidth / (48 * 2)) * 48 - 48 + 48 * 5,
       window.innerHeight - 48 * 6.5,
       1
     ).draw();
 
-    new GrassBlock(
+    new IronBlock(
       this.id,
       0,
       window.innerHeight - 48 * 10,
       Math.round(window.innerWidth / (48 * 2))
     ).draw();
 
-    new GrassBlock(
+    new IronBlock(
       this.id,
       Math.round(window.innerWidth / (48 * 2)) * 48 - (48 *2),
       window.innerHeight - 48 * 11,
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import MainFrame from "./common/MainFrame.js";
 import SkyBackground from "./common/background/SkyBackground.js";
 import ArenaEasy from "./common/block-arena/ArenaEasy.js";
-import GrassBlock from "./common/blocks/GrassBlock.js";
+import IronBlock from "./common/blocks/IronBlock.js";
 import Player from "./common/character/Player.js";
 
 const frame = new MainFrame();
@@ -10,7 +10,7 @@ const idCanvas = "gameCanvas";
 
 frame.preload(async (loader) => {
   await SkyBackground.preload(loader);
-  await GrassBlock.preload(loader);
+  await IronBlock.preload(loader);
   await Player.preload(loader);
 });
 
@@ -19,11 +19,10 @@ frame.initialize((controller) => {
     skyBackground: new SkyBackground(idCanvas),
     blocks: new ArenaEasy(idCanvas),
     player: new Player(idCanvas, controller),
-  }
+  };
 });
 
 frame.update((renderer) => {
-  const { controller } = frame.getState();
   renderer.skyBackground.draw();
   renderer.blocks.draw();
   renderer.player.draw();
