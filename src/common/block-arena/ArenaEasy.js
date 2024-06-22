b50bc3c185fa61b9abc7b473e585f0605961fb2f
ferdian9991 <ferdisatria15@gmail.com>
2024-06-22 20:08:30 +0700

    (change) new arena block

--- src/common/block-arena/ArenaEasy.js
+++ src/common/block-arena/ArenaEasy.js
@@ -5,7 +5,7 @@ export default class ArenaEasy {
     this.id = id;
 
     this.block = new IronBlock(this.id);
-    window.blockPosition = window.gameCanvasObject.canvas.height - (48 * 4);
+    window.blockPosition = window.gameCanvasObject.canvas.height - 48 * 13;
   }
 
   draw() {
@@ -13,33 +13,67 @@ export default class ArenaEasy {
       {
         x: 0,
         y: window.gameCanvasObject.canvas.height - 48,
-        drawTimes: Math.round(window.gameCanvasObject.canvas.width / 48) + 1,
+        drawTimes: window.gameCanvasObject.canvas.width / 48,
       },
       {
-        x: 0,
-        y: window.gameCanvasObject.canvas.height - 48 * 5.5,
-        drawTimes:
-          Math.round(window.gameCanvasObject.canvas.width / (48 * 2)) + 5,
+        x: 48 * 5,
+        y: window.gameCanvasObject.canvas.height - 48 * 2,
+        drawTimes: 1,
       },
       {
-        x:
-          Math.round(window.gameCanvasObject.canvas.width / (48 * 2)) * 48 -
-          48 +
-          48 * 5,
-        y: window.gameCanvasObject.canvas.height - 48 * 6.5,
+        x: 48 * 6,
+        y: window.gameCanvasObject.canvas.height - 48 * 2,
+        drawTimes: 1,
+      },
+      {
+        x: 48 * 6,
+        y: window.gameCanvasObject.canvas.height - 48 * 3,
+        drawTimes: 1,
+      },
+      {
+        x: 48 * 7,
+        y: window.gameCanvasObject.canvas.height - 48 * 2,
+        drawTimes: 1,
+      },
+      {
+        x: 48 * 7,
+        y: window.gameCanvasObject.canvas.height - 48 * 3,
+        drawTimes: 1,
+      },
+      {
+        x: 48 * 7,
+        y: window.gameCanvasObject.canvas.height - 48 * 4,
+        drawTimes: (window.gameCanvasObject.canvas.width - 48 * 10) / 48,
+      },
+      {
+        x: window.gameCanvasObject.canvas.width - 48 * 2,
+        y: window.gameCanvasObject.canvas.height - 48 * 2,
+        drawTimes: 2,
+      },
+      {
+        x: window.gameCanvasObject.canvas.width - 48,
+        y: window.gameCanvasObject.canvas.height - 48 * 3,
         drawTimes: 1,
       },
       {
         x: 0,
-        y: window.gameCanvasObject.canvas.height - 48 * 10,
-        drawTimes: Math.round(window.gameCanvasObject.canvas.width / (48 * 2)),
+        y: window.gameCanvasObject.canvas.height - 48 * 6,
+        drawTimes: 5,
       },
       {
-        x:
-          Math.round(window.gameCanvasObject.canvas.width / (48 * 2)) * 48 -
-          48 * 2,
+        x: 48 * 7,
+        y: window.gameCanvasObject.canvas.height - 48 * 8,
+        drawTimes: (window.gameCanvasObject.canvas.width - 48 * 7) / 48,
+      },
+      {
+        x: 48 * 11,
+        y: window.gameCanvasObject.canvas.height - 48 * 9,
+        drawTimes: 5,
+      },
+      {
+        x: 48 * 18,
         y: window.gameCanvasObject.canvas.height - 48 * 11,
-        drawTimes: 1,
+        drawTimes: (window.gameCanvasObject.canvas.width - 48 * 18) / 48,
       },
     ]);
   }
