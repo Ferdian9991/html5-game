a82967405f6d56f16d76b14be992fc925bece307
Ferdian9991 <ferdisatria15@gmail.com>
2024-06-22 13:35:24 +0700

    (fix) block following canvas size

--- src/common/block-arena/ArenaEasy.js
+++ src/common/block-arena/ArenaEasy.js
@@ -11,19 +11,19 @@ export default class ArenaEasy {
       this.id,
       0,
       window.innerHeight - 48,
-      Math.round(window.innerWidth / 48) + 1
+      Math.round(window.gameCanvasObject.canvas.width / 48) + 1
     ).draw();
 
     new IronBlock(
       this.id,
       0,
       window.innerHeight - 48 * 5.5,
-      Math.round(window.innerWidth / (48 * 2)) + 5
+      Math.round(window.gameCanvasObject.canvas.width / (48 * 2)) + 5
     ).draw();
 
     new IronBlock(
       this.id,
-      Math.round(window.innerWidth / (48 * 2)) * 48 - 48 + 48 * 5,
+      Math.round(window.gameCanvasObject.canvas.width / (48 * 2)) * 48 - 48 + 48 * 5,
       window.innerHeight - 48 * 6.5,
       1
     ).draw();
@@ -32,12 +32,12 @@ export default class ArenaEasy {
       this.id,
       0,
       window.innerHeight - 48 * 10,
-      Math.round(window.innerWidth / (48 * 2))
+      Math.round(window.gameCanvasObject.canvas.width / (48 * 2))
     ).draw();
 
     new IronBlock(
       this.id,
-      Math.round(window.innerWidth / (48 * 2)) * 48 - (48 *2),
+      Math.round(window.gameCanvasObject.canvas.width / (48 * 2)) * 48 - (48 *2),
       window.innerHeight - 48 * 11,
       1
     ).draw();
